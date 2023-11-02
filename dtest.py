1f95352af018fc23699f3947e206385a064c71af
NandanReddy Parnapalli <117740904+nandanreddyp@users.noreply.github.com>
2023-11-03 01:40:22 +0530

    Delete dtest.py

--- dtest.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-# print('hi',os.listdir('uploads'))
-# for song in os.listdir('uploads'):
-#     print(song, os.path.join(os.path.abspath(song))
-for x in os.walk('uploads'):
-    print(x)
