528019e9493bf8ee0b069b5164a572ba475dc8ef
NandanReddyP <parnapalli2004@gmail.com>
2023-11-19 23:57:16 +0530

    smal

--- Musica/functions.py
+++ Musica/functions.py
@@ -1,7 +1,7 @@
 # create admin
 def create_admin():
     from Musica.database.models import db, User
-    user = User(id='admin@musica',password=hash(12345678),f_name='Admin',role='admin')
+    user = User(id='admin@musica',password=hash('12345678'),f_name='Admin',role='admin')
     db.session.add(user)
     db.session.commit()
     print('admin created')
