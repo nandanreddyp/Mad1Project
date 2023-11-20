0c5e064df6205ebeee3f02082fd3ae8554246519
NandanReddyP <parnapalli2004@gmail.com>
2023-11-20 14:57:37 +0530

    kk

--- test.py
+++ test.py
@@ -3,12 +3,3 @@ from Musica.functions import get_linked_list, create_upload_folders
 from Musica.database.models import User, db
 
 
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-y = np.array([35, 25, 25, 15])
-mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
-
-plt.pie(y, labels = mylabels)
-plt.show() 
